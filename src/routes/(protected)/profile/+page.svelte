6dbdf4d0d8a6a214a814842f66885dccb332d3f1
Prabhas Kurapati <prabhask@berkeley.edu>
2026-02-03 17:10:50 -0500

    [BUGFIX] DONE WITH PROJECT NO MORE COMMITS

--- src/routes/(protected)/profile/+page.svelte
+++ src/routes/(protected)/profile/+page.svelte
@@ -1801,8 +1801,7 @@
     align-items: center;
     justify-content: space-between;
     padding: 1rem 0;
-    border-bottom: 1px solid rgba(108, 92, 231, 0.1);
-    margin-bottom: 1rem;
+    margin-bottom: 0.5rem;
   }
 
   .setting-info {
